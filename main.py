6cfd248348cc4d1aeadfe716567b9897c84129e2
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-10-11 14:23:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World"
-
-@app.route("/soma")
-def soma_valores():
-    x=10+10
-    return "Sua soma de 10+10="+str(x)
+    return "Hello World - Pedro Furtado - Lab DevOps"
 
 if __name__ == '__main__':
     app.run()
