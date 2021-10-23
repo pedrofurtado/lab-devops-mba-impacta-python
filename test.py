178db850d5d0da7694378f4edb1fe90a2a57fadb
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-10-23 11:05:11 -0300

    Update test.py

--- test.py
+++ test.py
@@ -5,9 +5,12 @@ class Test(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.result = self.app.get('/')
+        self.soma = self.app.get('/soma')
 
     def test_requisicao(self):
         self.assertEqual(self.result.status_code, 200)
+        self.assertEqual(self.soma.status_code, 200)
 
     def test_conteudo(self):
         self.assertEqual(self.result.data.decode('utf-8'), "Hello World - Pedro Furtado - Lab DevOps Impacta")
+        self.assertEqual(self.soma.data.decode('utf-8'), "Sua soma de 10+10=20")
